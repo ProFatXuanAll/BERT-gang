da29fdab5058ff2359c428b5d5e67fdcf7a584bb
ProFatXuanAll <ProFatXuanAll@gmail.com>
2020-02-20 22:08:46 +0800

    update bertgang/bertgang_data.py

--- README.md
+++ README.md
@@ -28,23 +28,15 @@ data
 
 ```py
 {
-  'segment_a': 'I like hung yu.',
-  'segment_b': 'I like dogs.',
+  'segment_a': 'Example segment 1.',
+  'segment_b': 'Example segment 2.',
 }
 ```
 
 - `albert.pickle`, `bert.pickle`, `roberta.pickle` files structure
 
 ```py
-{
-  # output tensor from model
-  # shape: (seq_len, hid_dim)
-  'output_embeds': [[0.0, ...,] [0.0,...], ...],
-
-  # segment indicator
-  'token_type_ids': [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-
-  # token boundary indicator
-  'token_bound_ids': [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]
-}
+# output tensor from model
+# shape: (seq_len, hid_dim)
+[[0.0, ...,] [0.0,...], ...]
 ```
--- bertgang/bertgang_data.py
+++ bertgang/bertgang_data.py
@@ -6,13 +6,18 @@ from __future__ import unicode_literals
 import os
 import pickle
 
+from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Tuple
+from typing import Union
 
 import torch
 import torch.utils
 import torch.utils.data
 
+from . import bertgang_config
+from . import bertgang_tokenizer
 from . import bertgang_util
 
 
@@ -20,23 +25,83 @@ class PreTrainData(torch.utils.data.Dataset):
     def __init__(
         self,
         path: str,
+        config: bertgang_config.PreTrainConfig,
+        tokenizer: bertgang_tokenizer.Tokenizer,
         ignore_files: Optional[List[str]] = None
     ) -> None:
 
+        if not isinstance(config, bertgang_config.PreTrainConfig):
+            raise TypeError(
+                'parameter `config` must be type `bertgang_config.PreTrainConfig`.')
+
+        self.config = config
+
+        if not isinstance(tokenizer, bertgang_tokenizer.Tokenizer):
+            raise TypeError(
+                'parameter `tokenizer` must be type `bertgang_tokenizer.Tokenizer`.')
+
+        self.tokenizer = tokenizer
         self.path = path
-        self.all_files = {
-            idx: file_name
-            for idx, file_name in enumerate(bertgang_util.list_all_files(path, ignore_files=ignore_files))
-        }
 
-    def __len__(self):
-        return len(self.all_files)
+        all_files = bertgang_util.list_all_files(path, ignore_files=ignore_files)
 
-    def __getitem__(self, index):
-        with open(f'{self.path}/{self.all_files[index]}', 'rb') as f:
-            obj = pickle.load(f)
+        self.idx_file_mapping = {}
 
-        obj['output_embeds'] = torch.FloatTensor(obj['output_embeds'])
-        obj['token_type_ids'] = torch.LongTensor(obj['token_type_ids'])
+        for idx, file_name in enumerate(all_files):
+            self.idx_file_mapping[idx] = file_name
+
+    def __len__(self) -> int:
+        return len(self.idx_file_mapping)
+
+    def __getitem__(
+        self,
+        idx: int
+    ) -> Dict[str, Union[str, torch.FloatTensor]]:
+        target_dir = f'{self.path}/{self.idx_file_mapping[idx]}'
+
+        with open(f'{target_dir}/original.pickle', 'rb') as f:
+            input_obj = pickle.load(f)
+
+        obj = self.tokenizer.encode_plus(
+            text=input_obj['segment_a'],
+            text_pair=input_obj['segment_b'],
+            add_special_tokens=True,
+            max_length=self.config.max_position_embeddings,
+            pad_to_max_length=True,
+            return_attention_mask=True,
+            return_overflowing_tokens=False,
+            return_special_tokens_mask=False,
+            return_tensors='pt',
+            return_token_type_ids=True
+        )
+
+        for teacher in self.config.teachers:
+            with open(f'{target_dir}/{teacher}.pickle', 'rb') as f:
+                obj[f'{teacher}_output_embeds'] = torch.FloatTensor(pickle.load(f))
 
         return obj
+
+    @staticmethod
+    def collate_fn(
+        obj_list: List[Dict]
+    ) -> Tuple[torch.LongTensor, torch.LongTensor, torch.FloatTensor]:
+        input_ids = []
+        token_type_ids = []
+        attention_mask = []
+
+        for obj in obj_list:
+            input_ids.append(obj['input_ids'].unsqueeze(0))
+            token_type_ids.append(obj['token_type_ids'].unsqueeze(0))
+            attention_mask.append(obj['attention_mask'].unsqueeze(0))
+
+        # TODO: return teacher tensor
+        return (torch.cat(input_ids),
+                torch.cat(token_type_ids),
+                torch.cat(attention_mask),)
+
+    def get_data_loader(self) -> None:
+        return torch.utils.data.DataLoader(self,
+                                           batch_size=self.config.batch_size,
+                                           collate_fn=self.__class__.collate_fn,
+                                           drop_last=False,
+                                           shuffle=True)
