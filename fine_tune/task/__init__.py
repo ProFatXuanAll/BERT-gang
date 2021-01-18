e66e146104a0cef73b15ac4fdda747ddf0fdde04
france5289 <st980407@gmail.com>
2021-01-18 15:03:40 +0800

    implement dataset base class and MNLI class for contrastive learning

--- fine_tune/task/__init__.py
+++ fine_tune/task/__init__.py
@@ -25,7 +25,9 @@ from __future__ import unicode_literals
 
 from fine_tune.task._boolq import BoolQ
 from fine_tune.task._dataset import Dataset
+from fine_tune.task._dataset import ContrastDataset
 from fine_tune.task._dataset import get_num_class
 from fine_tune.task._dataset import label_decoder
 from fine_tune.task._dataset import label_encoder
 from fine_tune.task._mnli import MNLI
+from fine_tune.task._mnli_contrast import MNLIContrast
