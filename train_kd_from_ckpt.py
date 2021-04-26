e48efe2c2f5d05f921f1f93475a42ad2d8eba5dd
france5289 <st980407@gmail.com>
2021-04-26 20:57:24 +0800

    Implement train classifier from checkpoint script

--- train_kd_from_ckpt.py
+++ train_kd_from_ckpt.py
@@ -1,8 +1,9 @@
-r"""It is a testing script.
-Use this script to find the best training schedule for SCL loss.
+r"""It is a temporary script.
+Use this script to train linear classifier on top of BERT student model.
 
 This script will load from a given checkpoint model which is assumed
-to be trained on MSE loss before, then train this model with SCL loss.
+to be trained on MSE loss and SCE loss before,
+then train this model with Cross Entropy loss.
 """
 
 # built-in modules
@@ -24,42 +25,67 @@ from tqdm import tqdm
 
 import fine_tune
 
-def train_SCL(
+def train_kd(
+    t_config: fine_tune.config.TeacherConfig,
     student_config: fine_tune.config.StudentConfig,
     train_dataset: fine_tune.task.Dataset,
+    t_model: fine_tune.model.TeacherModel,
     student_model: fine_tune.model.StudentModel,
-    scl_optimizer: torch.optim.AdamW,
-    scl_scheduler: torch.optim.lr_scheduler.LambdaLR,
+    kd_optimizer: torch.optim.AdamW,
+    kd_scheduler: torch.optim.lr_scheduler.LambdaLR,
+    teacher_tokenizer: transformers.PreTrainedTokenizer,
     student_tokenizer: transformers.PreTrainedTokenizer,
-    scl_temp: float = 0.1
+    alpha: float = 0.5,
+    softmax_temp: float = 1.0
 ):
-    """Train model with SCL loss.
+    """Train knowledge distillation loss from given fine-tuned teacher and
+    student model which has been trained on MSE and SCL loss.
+    This function may use two gpu-device.
+
+    Notes
+    ----------
+    .. math::
+        L_{KD} = (1-\alpha) * L_{CE}(y, y^s) + \alpha * L_{KL_Div}(y^t, t^s)
 
     Parameters
     ----------
+    t_config : fine_tune.config.TeacherConfig
+        `fine_tune.config.TeacherConfig` class which attributes are used
+        for experiment setup.
     student_config : fine_tune.config.StudentConfig
         `fine_tune.config.StudentConfig` class which attributes are used
         for experiment setup.
     train_dataset : fine_tune.task.Dataset
         Task specific dataset.
+    t_model : fine_tune.model.TeacherModel
+        A fine-tuned teacher model which is used to generate soft targets.
     student_model : fine_tune.model.StudentModel
-        Student model.
-    scl_optimizer : torch.optim.AdamW
+        Model which will perform disitllation according to
+        outputs from given teacher model.
+    kd_optimizer : torch.optim.AdamW
         `torch.optim.AdamW` optimizer.
-    scl_scheduler: torch.optim.lr_scheduler.LambdaLR
-        `torch.optim.lr_scheduler.LambdaLR` scheduler.
+    kd_scheduler : torch.optim.lr_scheduler.LambdaLR
+        Linear warmup scheduler provided by `transformers` package.
+    teacher_tokenizer : transformers.PreTrainedTokenizer
+        Tokenizer paired with `teacher_model`.
     student_tokenizer : transformers.PreTrainedTokenizer
         Tokenizer paired with `student_model`.
-    scl_temp : float, optional
-        Temperature of Supervised Contrastive Learning loss, by default 0.1
+    alpha : float, optional
+        Weight of soft target loss, by default 0.5
+    softmax_temp : float, optional
+        Softmax temperature, by default 1.0
     """
-    # Set model as training mode.
+
+    # Set teacher model as evaluation mode.
+    t_model.eval()
+
+    # Set student model as training mode.
     student_model.train()
 
     # Clean all gradient.
-    scl_optimizer.zero_grad()
+    kd_optimizer.zero_grad()
 
-    # Get experiment name and path for new model checkpoint.
+    # Get experiment name and path for student model.
     experiment_name = fine_tune.config.BaseConfig.experiment_name(
         experiment=student_config.experiment,
         model=student_config.model,
@@ -70,18 +96,13 @@ def train_SCL(
         experiment_name
     )
 
-    # Contruct custom batch sampler.
-    glue_sampler = fine_tune.task.GlueBatchSampler(
-        train_dataset,
-        batch_size=student_config.batch_size // student_config.accum_step
-    )
-
-    # Construct data loader.
+    # Teacher and student share a dataloader.
     dataloader = torch.utils.data.DataLoader(
         train_dataset,
-        batch_sampler=glue_sampler,
+        batch_size=student_config.batch_size // student_config.accum_step,
         collate_fn=train_dataset.create_collate_fn(),
         num_workers=os.cpu_count(),
+        shuffle=True
     )
 
     # Create tensorboard's `SummaryWriter`.
@@ -93,7 +114,7 @@ def train_SCL(
     )
 
     # Create objective functions.
-    scl_objective = fine_tune.contrast_util.SupConLoss(scl_temp)
+    criterion = fine_tune.objective.distill_loss
 
     # Accumulation step counter.
     step = 0
@@ -103,14 +124,14 @@ def train_SCL(
     # Mini-batch loss and accmulate loss.
     # Update when accumulate to `config.batch_size`.
     # For CLI and tensorboard logger.
-    scl_loss = 0
+    logits_loss = 0
 
     # torch.Tensor placeholder.
-    accum_scl_loss = 0
+    accum_logits_loss=0
 
-    # `tqdm` CLI logger. We will manually update progress bar.
+    # `tqdm` CLI Logger. We will manually update progress bar.
     cli_logger = tqdm(
-        desc=f'scl_loss: {scl_loss:.6f}',
+        desc=f'logits_loss: {logits_loss:.6f}',
         total=student_config.total_step
     )
 
@@ -123,7 +144,19 @@ def train_SCL(
             label = torch.LongTensor(label)
 
             # Get `input_ids`, `token_type_ids` and `attention_mask` via tokenizer.
-            batch_encode = student_tokenizer(
+            teacher_batch_encode = teacher_tokenizer(
+                text=text,
+                text_pair=text_pair,
+                padding='max_length',
+                max_length=t_config.max_seq_len,
+                return_tensors='pt',
+                truncation=True
+            )
+            teacher_input_ids = teacher_batch_encode['input_ids']
+            teacher_token_type_ids = teacher_batch_encode['token_type_ids']
+            teacher_attention_mask = teacher_batch_encode['attention_mask']
+
+            student_batch_encode = student_tokenizer(
                 text=text,
                 text_pair=text_pair,
                 padding='max_length',
@@ -131,40 +164,45 @@ def train_SCL(
                 return_tensors='pt',
                 truncation=True
             )
-            input_ids = batch_encode['input_ids']
-            token_type_ids = batch_encode['token_type_ids']
-            attention_mask = batch_encode['attention_mask']
+            student_input_ids = student_batch_encode['input_ids']
+            student_token_type_ids = student_batch_encode['token_type_ids']
+            student_attention_mask = student_batch_encode['attention_mask']
 
-            # Get hidden states from model.
-            _, hiddens, _ = student_model(
-                input_ids=input_ids.to(student_config.device),
-                token_type_ids=token_type_ids.to(student_config.device),
-                attention_mask=attention_mask.to(student_config.device),
-                return_hidden_and_attn=True
+            # Get soft targets.
+            with torch.no_grad():
+                teacher_logits, _ = t_model(
+                    input_ids = teacher_input_ids.to(t_config.device),
+                    token_type_ids=teacher_token_type_ids.to(t_config.device),
+                    attention_mask=teacher_attention_mask.to(t_config.device)
+                )
+
+            # Get ouput logits from student models.
+            student_logits, _ = student_model(
+                input_ids = student_input_ids.to(student_config.device),
+                token_type_ids=student_token_type_ids.to(student_config.device),
+                attention_mask=student_attention_mask.to(student_config.device)
             )
 
-            # Get [CLS] embedding of last layer.
-            last_cls = hiddens[-1][:,0,:]
-
-            # Calculate SCL loss.
-            accum_scl_loss = scl_objective(
-                features=last_cls,
-                labels=label.to(student_config.device)
+            accum_logits_loss = criterion(
+                hard_target=label.to(student_config.device),
+                teacher_logits=teacher_logits.to(student_config.device),
+                student_logits=student_logits,
+                alpha=alpha,
+                softmax_temp=softmax_temp
             )
-
             # Normalize loss.
-            accum_scl_loss = accum_scl_loss / student_config.accum_step
+            accum_logits_loss = accum_logits_loss / student_config.accum_step
 
             # Log loss.
-            scl_loss += accum_scl_loss.item()
+            logits_loss += accum_logits_loss.item()
 
             # Accumulate gradients.
-            accum_scl_loss.backward()
+            accum_logits_loss.backward()
 
             # Increment accumulation step.
             accum_step += 1
 
-            # Perform gradient descend when achieve actual mini-batch size.
+            # Peform gradient descend when achieve actual mini-batch.
             if accum_step % student_config.accum_step == 0:
                 # Gradient clipping.
                 torch.nn.utils.clip_grad_norm_(
@@ -173,15 +211,15 @@ def train_SCL(
                 )
 
                 # Gradient descend.
-                scl_optimizer.step()
+                kd_optimizer.step()
 
                 # Update learning rate.
-                scl_scheduler.step()
+                kd_scheduler.step()
 
                 # Log on CLI.
                 cli_logger.update()
                 cli_logger.set_description(
-                    f'scl_loss: {scl_loss:.6f}'
+                    f'logits_loss: {logits_loss:.6f}'
                 )
 
                 # Increment actual step.
@@ -191,21 +229,21 @@ def train_SCL(
                 if step % student_config.log_step == 0:
                     writer.add_scalar(
                         f'{student_config.task}/{student_config.dataset}/{student_config.model}'+
-                        '/scl_loss',
-                        scl_loss,
+                        '/logits_loss',
+                        logits_loss,
                         step
                     )
                     writer.add_scalar(
                         f'{student_config.task}/{student_config.dataset}/{student_config.model}/lr',
-                        scl_optimizer.state_dict()['param_groups'][0]['lr'],
+                        kd_optimizer.state_dict()['param_groups'][0]['lr'],
                         step
                     )
 
                 # Clean up mini-batch loss.
-                scl_loss = 0
+                logits_loss = 0
 
                 # Clean up gradient.
-                scl_optimizer.zero_grad()
+                kd_optimizer.zero_grad()
 
                 # Save model for each `student_config.ckpt_step`.
                 if step % student_config.ckpt_step == 0:
@@ -230,8 +268,11 @@ def train_SCL(
     )
 
 
+
+
+
 # Get main logger.
-logger = logging.getLogger('fine_tune.train_scl_from_ckpt')
+logger = logging.getLogger('fine_tune.train_ce_from_ckpt')
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
     datefmt='%Y/%m/%d %H:%M:%S',
@@ -247,6 +288,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Required parameters.
+    parser.add_argument(
+        '--teacher_exp',
+        help='Experiment name of the fine-tuned teacher model',
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        '--tmodel',
+        help='Name of the teacher model to transfer knowledge',
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        '--tckpt',
+        help='Checkpoint of teacher model to generate logits, hidden states and attentions',
+        required=True,
+        type=int,
+    )
     parser.add_argument(
         '--experiment',
         help='Name of the current fine-tune experiment.',
@@ -286,9 +345,21 @@ if __name__ == "__main__":
 
     # Optional arguments.
     parser.add_argument(
-        '--scl_temp',
-        default=0.1,
-        help='Temperature of Supervised Contrastive Loss.',
+        '--tdevice_id',
+        help='Device ID of teacher model. If not specified then load from config',
+        default=-1,
+        type=int
+    )
+    parser.add_argument(
+        '--softmax_temp',
+        default=1.0,
+        help='Temperature of softmax function.',
+        type=float
+    )
+    parser.add_argument(
+        '--soft_weight',
+        default=0.5,
+        help='Weight of soft target loss.',
         type=float
     )
     parser.add_argument(
@@ -384,6 +455,25 @@ if __name__ == "__main__":
     logger.info("Source model configuration:")
     logger.info(config)
 
+    # Load fine-tune teacher model configuration.
+    teacher_config = fine_tune.config.TeacherConfig.load(
+        experiment=args.teacher_exp,
+        model=args.tmodel,
+        task=args.task
+    )
+
+    # Sync batch size and accumulation steps.
+    teacher_config.batch_size = args.batch_size
+    teacher_config.accum_step = args.accum_step
+
+    # Set new device ID for teacher model if needed.
+    if args.tdevice_id > -1:
+        teacher_config.device_id = args.tdevice_id
+
+    # Log teacher configuration.
+    logger.info("Teacher model configuration:")
+    logger.info(teacher_config)
+
     # Control random seed for reproducibility.
     fine_tune.util.set_seed_by_config(
         config=config
@@ -395,6 +485,35 @@ if __name__ == "__main__":
     # Load training dataset.
     dataset = fine_tune.util.load_dataset_by_config(
         config=config
+    )
+
+    # Load teacher tokenizer and model.
+    teacher_tknr = fine_tune.util.load_teacher_tokenizer_by_config(
+        config=teacher_config
+    )
+    teacher_model = fine_tune.util.load_teacher_model_by_config(
+        config=teacher_config
+    )
+
+    # Load teacher model from given checkpoint.
+    texp_name = fine_tune.config.BaseConfig.experiment_name(
+        experiment=teacher_config.experiment,
+        model=teacher_config.model,
+        task=teacher_config.task
+    )
+    tmodel_name = os.path.join(
+        fine_tune.path.FINE_TUNE_EXPERIMENT,
+        texp_name,
+        f'model-{args.tckpt}.pt'
+    )
+
+    logger.info("Load teacher checkpoint from %s", tmodel_name)
+
+    teacher_model.load_state_dict(
+        torch.load(
+            tmodel_name,
+            map_location=teacher_config.device
+        )
     )
 
     # Load tokenizer and model.
@@ -443,7 +562,7 @@ if __name__ == "__main__":
     config.warmup_step = args.warmup_step
     config.weight_decay = args.weight_decay
 
-    logger.info("New config for SCL training")
+    logger.info("New config for KD training")
     logger.info(config)
     config.save()
 
@@ -460,12 +579,16 @@ if __name__ == "__main__":
     )
 
     # Start training.
-    train_SCL(
+    train_kd(
+        t_config=teacher_config,
         student_config=config,
         train_dataset=dataset,
+        t_model=teacher_model,
         student_model=model,
-        scl_optimizer=optimizer,
-        scl_scheduler=scheduler,
+        kd_optimizer=optimizer,
+        kd_scheduler=scheduler,
+        teacher_tokenizer=teacher_tknr,
         student_tokenizer=tokenizer,
-        scl_temp=args.scl_temp
+        alpha=args.soft_weight,
+        softmax_temp=args.softmax_temp
     )
